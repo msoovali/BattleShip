19a6c712a44a30150c1bdc12662f991231213c78
Martin Sooväli <msoovali@gmail.com>
2019-02-21 23:37:54 +0200

    Added wwwroot
    Styling was missing, now it's in repo

---
+++ WebApp/wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+$(function () {
+    $('#AICheckbox').click(function () {
+        var cb1 = $('#AICheckbox').is(':checked');
+        $('#PlayerTwo').prop('disabled', cb1);
+    });
+});
+
+$(function () {
+    $('#custom-ships').click(function () {
+        var cb2 = $('#custom-ships').is(':checked');
+        $('#no-of-ships').prop('disabled', cb2);
+    });
+});
